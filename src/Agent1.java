8041b024864ca2521866c427a7cb1992cd1726b9
Yohan Michelland <yohan.michelland@etu.univ-lyon1.fr>
2021-10-20 17:01:33 +0200

    premiers tests agents

--- src/Agent1.java
+++ src/Agent1.java
@@ -1,4 +1,24 @@
 import jade.core.Agent;
+import jade.lang.acl.ACLMessage;
 
 public class Agent1 extends Agent {
+
+    protected String name;
+
+    public Agent1(String _name) {
+        super();
+        name = _name;
+    }
+
+    protected void setup() {
+        sendMessage(this.getLocalName());
+        System.out.println(name);
+
+    }
+
+    protected void sendMessage(String mess) {
+        ACLMessage inform = new ACLMessage(ACLMessage.INFORM);
+        inform.setContent(mess);
+        inform.setProtocol("information");
+    }
 }
