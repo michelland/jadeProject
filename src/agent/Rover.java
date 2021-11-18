a78b378974887392f6841f7b5316064971ddf71b
main pc <favre.victor@protonmail.com>
2021-11-18 09:48:37 +0100

    from the bottom

--- src/agent/Rover.java
+++ src/agent/Rover.java
@@ -4,6 +4,7 @@ import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.FSMBehaviour;
 import jade.core.behaviours.OneShotBehaviour;
 import jade.lang.acl.ACLMessage;
 import javafx.util.Pair;
@@ -14,13 +15,20 @@ import world.Type;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Vector;
 
 
 public class Rover extends Agent {
 
     protected String name;
-    protected State state;
+    public State state;
+    public ACLMessage msg;
+    public int battery_pourcentage = 100;
+    public int nb_sample = 0;
+    public int heure = 0;
+
+
 
     @Override
     protected void setup() {
@@ -28,9 +36,11 @@ public class Rover extends Agent {
         int x = (int) (Math.random() * (Planet.SIZE));
         int y = (int) (Math.random() * (Planet.SIZE));
         state = new State(x,y);
+
+
         state.setStatus(Status.RUNNING);
 
-        addBehaviour(new CyclicBehaviour() {
+        /*addBehaviour(new CyclicBehaviour() {
 
             @Override
             public void action() {
@@ -54,8 +64,26 @@ public class Rover extends Agent {
                 }
                 //block();
             }
-        });
+        });*/
 
+        FSMBehaviour fsm = new FSMBehaviour(this);
+        fsm.registerFirstState(new startBehaviour(),"start");
+        fsm.registerFirstState(new exploring(this),"exploring");
+
+
+
+
+        fsm.registerDefaultTransition("start","exploring");
+        fsm.registerTransition("exploring", "exploring", 0);
+
+
+    }
+    private class startBehaviour extends OneShotBehaviour {
+
+        @Override
+        public void action() {
+            System.out.println("c'est parti");
+        }
     }
 
     @Override
@@ -97,4 +125,63 @@ public class Rover extends Agent {
             }
         }
     }
+    private static class receiving extends OneShotBehaviour{
+
+        Rover agent;
+
+        public receiving(Rover _a){
+            this.agent = _a;
+        }
+
+        @Override
+        public void action() {
+            agent.msg = agent.receive();
+        }
+    }
+
+
+    private class exploring extends OneShotBehaviour{
+
+        Rover rover;
+        int next_state = 0;
+
+        public exploring(Rover _rover) {
+            rover = _rover;
+        }
+
+        @Override
+        public void action() {
+            String content_msg = "getinfo:" + rover.state.getX() + "," + rover.state.getY();
+            sendMessage(content_msg, "Planet");
+
+            rover.msg = rover.blockingReceive();
+
+            String[] content = rover.msg.getContent().split("-");
+            if(rover.battery_pourcentage < 10){
+                next_state = 1; // go to recharging
+            }
+            else if(Objects.equals(content[0], "help")){
+                next_state = 2; // go to help
+                //TODO add the parse of a position and the varaible in rover to stotre this position
+            }
+            else if(Objects.equals(content[0], "sample")){
+                next_state = 3; // go to gathering
+            }
+            else if(Objects.equals(content[0], "mouvement")){
+                //TODO add the code to choose in what direction go
+                //binary number on each direction summed to have a int as a sup up of direction
+                // 1    or perhaps juste a list inside []
+                //8 2
+                // 4
+                next_state = 0; // redo this state
+            }
+        }
+
+        public int onEnd(){
+            return next_state;
+        }
+    }
+
 }
+
+
